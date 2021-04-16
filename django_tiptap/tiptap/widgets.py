8db848a9736062b666ec7192914019f95efcccf9
Jeet Mandaliya <45892659+sereneinserenade@users.noreply.github.com>
2021-04-16 22:24:44 +0200

    implementation done

--- django_tiptap/tiptap/widgets.py
+++ django_tiptap/tiptap/widgets.py
@@ -8,7 +8,6 @@ class TipTapWidget(forms.Textarea):
             # "//cdn.skypack.dev/@tiptap/starter-kit?min",
             # "js/tiptap-min.js",
         )
-        # html = ("html/index.html",)
 
     template_name = "forms/tiptap_textarea.html"
 
