dd31d7b778e605ddb471646f6f55271dd84cf754
Max Patzelt <max.patzelt@legislayer.com>
2021-04-23 14:43:51 +0200

    Add more configuration options

--- django_tiptap/config.py
+++ django_tiptap/config.py
@@ -1,4 +1,22 @@
 TIPTAP_DEFAULT_CONFIG = {
     "width": "500px",
     "height": "500px",
+    "extensions": [
+        "bold",
+        "italic",
+        "underline",
+        "strikethrough",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "textAlign",
+        "indent",
+        "table",
+        "typography",
+    ],
+    "placeholderText": "Begin typing here...",
+    "unsavedChangesWarningText": "You have unsaved changes",
 }
