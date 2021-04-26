884e2fc59661362bcccfda1ae23689021836bc32
Max Patzelt <max.patzelt@legislayer.com>
2021-04-26 15:33:36 +0200

    Move static files into namespaced subfolder

--- django_tiptap/widgets.py
+++ django_tiptap/widgets.py
@@ -8,7 +8,7 @@ from .config import TIPTAP_DEFAULT_CONFIG
 
 class TipTapWidget(forms.Textarea):
     class Media:
-        css = {"all": ("css/styles-min.css",)}
+        css = {"all": ("django_tiptap/css/styles-min.css",)}
 
     template_name = "forms/tiptap_textarea.html"
 
