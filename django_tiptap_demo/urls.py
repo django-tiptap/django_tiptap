47790103ccd5e0db7d4271bca20ed079f4d00bcf
Lilith Wittmann <mail@lilithwittmann.de>
2022-12-08 22:33:07 +0100

    add simple link

--- django_tiptap_demo/urls.py
+++ django_tiptap_demo/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [path("admin/", admin.site.urls),] + static(
-    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
